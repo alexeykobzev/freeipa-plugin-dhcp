9481de6ea6e25d7641a8ef2affc5c03b588f3cc9
Alexey Kobzev <akobzev@vigo.one>
2022-09-20 18:54:35 +0300

    show all words

--- plugin/ipaserver/dhcpv4.py
+++ plugin/ipaserver/dhcpv4.py
@@ -1988,17 +1988,40 @@ class dhcphost(LDAPObject):
 
     takes_params = (
         Str(
-            'cn',
+            'cn?',
             cli_name='cn',
             label=_('Canonical Name'),
             doc=_('Canonical name.'),
             primary_key=True
         ),
         Str(
-            'dhcphwaddress',
-            cli_name='dhcphwaddress',
-            label=('DHCP Hardware Address'),
-            doc=_('DHCP hardware address.')
+            'fqdn',
+            cli_name='fqdn',
+            label=_('Host Name'),
+            doc=_('Host name.'),
+            flags=['virtual_attribute']
+        ),
+        Str('macaddress*',
+            normalizer=lambda value: value.upper(),
+            pattern='^([a-fA-F0-9]{2}[:|\-]?){5}[a-fA-F0-9]{2}$',
+            pattern_errmsg=('Must be of the form HH:HH:HH:HH:HH:HH, where '
+                            'each H is a hexadecimal character.'),
+            label=_('MAC address'),
+            doc=_('Hardware MAC address(es) on this host'),
+            flags=['virtual_attribute']
+        ),
+        Str(
+            'ipaddress?',
+            cli_name='ipaddress',
+            label=_('Host IP Address'),
+            doc=_('Host IP Address.'),
+            flags=['virtual_attribute']
+        ),
+        Str(
+            'dhcpclientid?',
+            cli_name='dhcpclientid',
+            label=_('Client Identifier'),
+            doc=_('Client Identifier.')
         ),
         Str(
             'dhcpstatements*',
@@ -2016,8 +2039,8 @@ class dhcphost(LDAPObject):
             'dhcpcomments?',
             cli_name='dhcpcomments',
             label=_('Comments'),
-            doc=_('DHCP comments.')
-        )
+            doc=_('DHCP Comments.')
+        ),
     )
 
     @staticmethod
@@ -2121,6 +2144,30 @@ class dhcphost_add(Command):
             cli_name='ipaddress',
             label=_('IP Address'),
             doc=_("Host IP Address.")
+        ),
+        Str(
+            'dhcphwaddress',
+            cli_name='dhcphwaddress',
+            label=('DHCP Hardware Address'),
+            doc=_('DHCP hardware address.')
+        ),
+        Str(
+            'dhcpstatements*',
+            cli_name='dhcpstatements',
+            label=_('DHCP Statements'),
+            doc=_('DHCP statements.')
+        ),
+        Str(
+            'dhcpoption*',
+            cli_name='dhcpoptions',
+            label=_('DHCP Options'),
+            doc=_('DHCP options.')
+        ),
+        Str(
+            'dhcpcomments?',
+            cli_name='dhcpcomments',
+            label=_('Comments'),
+            doc=_('DHCP comments.')
         )
     )
 
