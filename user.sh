09a91697631d79c15334c85b6b40c7de0fd3614d
Alexey Kobzev <akobzev@vigo.one>
2022-10-13 13:52:38 +0300

    try to fix permissions

--- user.sh
+++ user.sh
@@ -5,7 +5,7 @@ kinit admin@VIGO.INTERNAL
 
 ### add the user and set privileges to LDAP DIT
 ldapmodify -X admin -D "cn=Directory Manager" -W -p 389 -h ipaserver.vigo.internal -a -f user/dhcpd_account.ldif
-ldapmodify -X admin -D "cn=Directory Manager" -W -p 389 -h ipaserver.vigo.internal -a -f user/dhcpd_privileges.ldif
+#ldapmodify -X admin -D "cn=Directory Manager" -W -p 389 -h ipaserver.vigo.internal -a -f user/dhcpd_privileges.ldif
 
 ### set a decent password for the newly created system account
 ldappasswd -X admin -S -W -D "cn=Directory Manager" uid=dhcp,cn=sysaccounts,cn=etc,dc=vigo,dc=internal
